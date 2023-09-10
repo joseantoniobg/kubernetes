5ba7fecf25f8c29c1e505c86435001b26101283f
José Antônio <joseantonio1@hotmail.com.br>
2023-09-10 16:49:12 -0300

    a

--- server.go
+++ server.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+	"fmt"
+)
 
 func main()  {
 		http.HandleFunc("/", Hello)
@@ -8,5 +12,8 @@ func main()  {
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>HELLO WORLD!!!!</h1>"))
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+
+  fmt.Fprintf(w, "Hello! I`m %s. I`m %s", name, age)
 }
