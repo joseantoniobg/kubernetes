23dc74a22a93e87d66339fa6c3c0d45114509d02
José <joseantonio1@hotmail.com.br>
2023-09-11 15:30:17 -0300

    feat: new kubernetes features like env and secrets

--- server.go
+++ server.go
@@ -4,9 +4,13 @@ import (
 	"net/http"
 	"os"
 	"fmt"
+	"io/ioutil"
+	"log"
 )
 
 func main()  {
+		http.HandleFunc("/secret", Secret)
+		http.HandleFunc("/configmap", ConfigMap)
 		http.HandleFunc("/", Hello)
 		http.ListenAndServe(":8001", nil)
 }
@@ -16,4 +20,21 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	age := os.Getenv("AGE")
 
   fmt.Fprintf(w, "Hello! I`m %s. I`m %s", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("myfamily/family.txt")
+
+	if err != nil {
+		log.Fatalf("Error reading File", err)
+	}
+
+  fmt.Fprintf(w, "My family: %s", string(data))
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+
+  fmt.Fprintf(w, "User: %s Password: %s", user, password)
 }
